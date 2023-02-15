df62212614f990d2f5a431ad44f1084025736fc7
Ali-MHoseini <ali.mhoseini99@gmail.com>
2023-02-15 21:48:26 +0330

    fix:build/FileStructure

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,25 @@
-import {Route} from 'react-router-dom'
+import {Route, Routes} from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
       <>
-            <Route path='/'/>
-            <Route path='/cart'/>
-            <Route path='/login'/>
-            <Route path='/payment_result'/>
-            <Route path='/products' children='/:id'/>
-            <Route path='/purchase'/>
-          <Route path='/dashboard'>
-              <Route path='/inventory'/>
-              <Route path='/products_manage'/>
-              <Route path='/order'/>
-          </Route>
-            <Route path='/'/>
-      </>
+          <Routes>
+              <Route path='/' />
+              <Route path='/cart'/>
+              <Route path='/login'/>
+              <Route path='/payment_result'/>
+              <Route path='/products' children='/:id'/>
+              <Route path='/purchase'/>
+              <Route path='/dashboard'>
+                  <Route path='/inventory'/>
+                  <Route path='/products_manage'/>
+                  <Route path='/order'/>
+              </Route>
+              <Route path='*'/>
+          </Routes>
+          </>
   )
 
 
