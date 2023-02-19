de5c76212cd9b3f5302f73435b935daa43c6b24d
Ali-MHoseini <ali.mhoseini99@gmail.com>
2023-02-19 19:21:17 +0330

    build/categoryBox Finished

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,6 +1,8 @@
 import '../../assets/styles/index.css'
 import {Slide} from 'react-slideshow-image';
 import 'react-slideshow-image/dist/styles.css';
+import {Link, useNavigate} from 'react-router-dom';
+import {CardCategory} from "../../components/CardCategory/CardCategory";
 
 
 const divStyle = {
@@ -22,8 +24,42 @@ const slideImages = [
     },
 ];
 
+const cardCategoryList:category[] = [
+    {
+        name:'کتاب ها',
+        navigate:'/products/books',
+        img:'../../src/assets/images/ketab.png',
+    },{
+        name:'لوازم خانگی',
+        navigate:'/products/housestuff',
+        img:'../../src/assets/images/lavazem-khanegi.png'
+    },{
+        name:'لوازم الکترونیکی',
+        navigate:'/products/electronicstuff',
+        img:'../../src/assets/images/Tech.png'
+    },{
+        name:'لوازم مسافرتی',
+        navigate:'/products/travelstuff',
+        img:'../../src/assets/images/travel.png'
+    },{
+        name:'خوراکی ها',
+        navigate:'/products/foods',
+        img:'../../src/assets/images/Foods.jpg'
+    },
+]
+type category = {
+    name:string,
+    navigate:string,
+    img:string
+
+
+}
 
 export const Homepage = () => {
+    const navigate = useNavigate()
+    const navFunc = (address:string):void => {
+        navigate(address)
+    }
     return(
         <div className="homepage">
             <div className='imgDesBox'>
@@ -35,6 +71,20 @@ export const Homepage = () => {
                         </div>
                     ))}
                 </Slide>
+            </div>
+            <div>
+                <h2 style={{fontSize:'2rem'}}>دسته بندی محصولات ما</h2>
+                <div className='category__list'>
+                    {
+                        cardCategoryList && cardCategoryList.map((category,index:number) => (
+                            <CardCategory
+                                key={index}
+                                Image={category.img}
+                                Name={category.name}
+                                NavFunc={()=>navFunc(category.navigate)}/>
+                        ))
+                    }
+                </div>
             </div>
 
         </div>
