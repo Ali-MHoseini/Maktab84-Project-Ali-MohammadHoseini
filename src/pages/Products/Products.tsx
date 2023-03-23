be018b05465257fedd1e82a6700e8ecd16a532c1
Ali-MHoseini <ali.mhoseini99@gmail.com>
2023-03-23 19:40:21 +0430

    add data to productsPage

--- src/pages/Products/Products.tsx
+++ src/pages/Products/Products.tsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import Button from "@mui/material/Button";
 import Pagination from '@mui/material/Pagination'
 import {Cards} from '../../components/Cards/Cards'
+import {getProds} from "../../middleware/api/api";
 
 export const Products = ()=> {
+    const [data,setData] = useState<any>([])
+    const [numOfPages,setNumOfPages] = useState<number>(1)
+    const [getData,setGetData] = useState<boolean>(false)
+    useEffect(() => {
+        getProds(1,6).then(res=> {
+            setNumOfPages(res.data.nbPages)
+            setData(res.data.hits)})
+    }, []);
+    const changePage = (pageNum:string) => {
+        getProds(pageNum,6).then(res=> setData(res.data.hits))
+    }
+
     return(
         <div className='productsPage'>
             <div className='categoryBox'>
@@ -17,17 +30,21 @@ export const Products = ()=> {
                     <Button >گران ترین</Button>
                 </div>
                 <div className='productsSec'>
-                    <Cards Image='#' Title='گوشی ایفون 14' Price='20000' key={crypto.randomUUID()} id='#'/>
-                    <Cards Image='#' Title='گوشی ایفون 14' Price='20000' key={crypto.randomUUID()} id='#'/>
-                    <Cards Image='#' Title='گوشی ایفون 14' Price='20000' key={crypto.randomUUID()} id='#'/>
-                    <Cards Image='#' Title='گوشی ایفون 14' Price='20000' key={crypto.randomUUID()} id='#'/>
-                    <Cards Image='#' Title='گوشی ایفون 14' Price='20000' key={crypto.randomUUID()} id='#'/>
-                    <Cards Image='#' Title='گوشی ایفون 14' Price='20000' key={crypto.randomUUID()} id='#'/>
+                    {
+                        data && data.map((item:any) => (
+                            <Cards
+                                Image={item.image}
+                                Title={item.name}
+                                Price={item.price}
+                                key={crypto.randomUUID()}
+                                id={item._id}/>
+                        ))
+                    }
                 </div>
                 <div style={{display:'flex',justifyContent:'center',direction:'ltr'}}>
                     <Pagination
-                        count={10}
-                        onClick={(e:React.MouseEvent) : void => console.log((e.target as HTMLButtonElement).innerText)}
+                        count={numOfPages}
+                        onClick={(e:React.MouseEvent) : void => changePage((e.target as HTMLButtonElement).innerText)}
                         showFirstButton showLastButton />
                 </div>
             </div>
